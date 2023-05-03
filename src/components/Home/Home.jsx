b311fec78bbfc57f1d0e37f05b4aaaa5b68c7fcc
Md-arefin <ansariarefin617@gmail.com>
2023-05-03 19:35:17 +0600

    implemented carousel on the next sec

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Carousel } from 'react-bootstrap';
 
-
 const Home = () => {
     return (
         <div>
@@ -24,7 +23,10 @@ const Home = () => {
             </div>
 
             <div className='container my-5 py-5 rounded bg-danger bg-opacity-50 text-center'>
-                <h3 className='mb-5 fs-2'>Our Famous Recipes</h3>
+                
+                <h3 className='mb-5 fs-2'>
+                    Our Famous Recipes
+                </h3>
 
                 <Carousel className='px-5 mx-auto'>
                     <Carousel.Item>
@@ -39,8 +41,8 @@ const Home = () => {
 
 
                             <div className='px-5 '>
-                                <h3 className='fs-3'>First slide label</h3>
-                                <p className='fs-5'>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                                <h3  className='fs-3 fw-bold'>Manti</h3>
+                                <p className='fs-5'><span  className='fs-4 fw-bold'>Manti</span> is a traditional Turkish dish that consists of small dumplings typically filled with spiced ground lamb or beef and served with a yogurt sauce and melted butter or tomato sauce. The dumplings are usually made by hand and can be served boiled or baked.</p>
                             </div>
 
                         </div>
@@ -49,99 +51,104 @@ const Home = () => {
 
                     <Carousel.Item>
                         <div className='d-flex'>
-                        <img
-                            className="d-block w-50 h-25  rounded"
-                            src="https://i.ibb.co/X2ktQnh/Sea-bass.jpg"
-                            alt="Second slide"
-                        />
+                            <img
+                                className="d-block w-50 h-25  rounded"
+                                src="https://i.ibb.co/X2ktQnh/Sea-bass.jpg"
+                                alt="Second slide"
+                            />
 
-                        <div className='px-5 '>
-                            <h3 className='fs-3'>First slide label</h3>
-                            <p className='fs-5'>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-                        </div>
+                            <div className='px-5 '>
+                                <h3  className='fs-3 fw-bold'>Sea Bass</h3>
+                                <p className='fs-5'><span  className='fs-4 fw-bold'>Sea Bass</span> refers to several species of fish that are popular in both commercial and recreational fishing. These fish are known for their mild, white flesh and are often considered a delicacy. Some common species of sea bass include the European sea bass, the black sea bass, and the Chilean sea bass.</p>
+                            </div>
                         </div>
                     </Carousel.Item>
                     <Carousel.Item>
                         <div className='d-flex'>
-                        <img
-                            className="d-block w-50 h-25  rounded  "
-                            src="https://i.ibb.co/314Xt59/Baklava-Recipe-16.jpg"
-                            alt="Third slide"
-                        />
+                            <img
+                                className="d-block w-50 h-25  rounded  "
+                                src="https://i.ibb.co/314Xt59/Baklava-Recipe-16.jpg"
+                                alt="Third slide"
+                            />
 
-                        <div className='px-5 '>
-                            <h3 className='fs-3'>First slide label</h3>
-                            <p className='fs-5'>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-                        </div>
+                            <div className='px-5 '>
+                                <h3  className='fs-3 fw-bold'>Baklava</h3>
+                                <p className='fs-5'><span  className='fs-4 fw-bold'>Baklava</span> is a sweet pastry that is popular in many Middle Eastern and Mediterranean countries, including Turkey, Greece, and Lebanon. The pastry is made by layering phyllo dough with a mixture of chopped nuts, such as pistachios, walnuts, or almonds, and a sweet syrup or honey. After baking, the pastry is cut into small diamond-shaped pieces and served as a dessert.
+                                </p>
+                            </div>
                         </div>
                     </Carousel.Item>
                     <Carousel.Item>
                         <div className='d-flex'>
-                        <img
-                            className="d-block w-50 h-25  rounded  "
-                            src="https://i.ibb.co/HPGJ24Q/best-lamb-kebab-recipe.jpg"
-                            alt="First slide"
-                        />
+                            <img
+                                className="d-block w-50 h-25  rounded  "
+                                src="https://i.ibb.co/HPGJ24Q/best-lamb-kebab-recipe.jpg"
+                                alt="First slide"
+                            />
 
-                        <div className='px-5 '>
-                            <h3 className='fs-3'>First slide label</h3>
-                            <p className='fs-5'>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-                        </div>
-                        </div>
-                    </Carousel.Item>
-                    <Carousel.Item>
-                       <div className='d-flex'>
-                       <img
-                            className="d-block w-50 h-25  rounded  "
-                            src="https://i.ibb.co/dsTKyN4/baklava-recipe-3.jpg"
-                            alt="Second slide"
-                        />
+                            <div className='px-5 '>
+                                <h3  className='fs-3 fw-bold'>Lamb Kebab</h3>
+                                <p className='fs-5'><span  className='fs-4 fw-bold'>Lamb Kebab</span> is a popular dish that consists of grilled or roasted lamb meat that is typically served on skewers or with a variety of accompaniments, such as rice, vegetables, and bread. The meat used in lamb kebab is usually marinated in a mixture of spices, herbs, and other ingredients to add flavor and tenderize the meat before it is cooked.
 
-                        <div className='px-5 '>
-                            <h3 className='fs-3'>First slide label</h3>
-                            <p className='fs-5'>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                                </p>
+                            </div>
                         </div>
-                       </div>
                     </Carousel.Item>
                     <Carousel.Item>
                         <div className='d-flex'>
-                        <img
-                            className="d-block w-50 h-25 rounded  "
-                            src="https://i.ibb.co/k3pR4pr/Manti-84d39c9632cf40fcb0ca7625d63556b5.jpg"
-                            alt="Third slide"
-                        />
+                            <img
+                                className="d-block w-50 h-25  rounded  "
+                                src="https://i.ibb.co/dsTKyN4/baklava-recipe-3.jpg"
+                                alt="Second slide"
+                            />
 
-                        <div className='px-5 '>
-                            <h3 className='fs-3'>First slide label</h3>
-                            <p className='fs-5'>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-                        </div>
+                            <div className='px-5 '>
+                                <h3  className='fs-3 fw-bold'>Baklava</h3>
+                                <p className='fs-5'>
+                                <span  className='fs-4 fw-bold'>Baklava</span> is a sweet pastry that is popular in many Middle Eastern and Mediterranean countries, including Turkey, Greece, and Lebanon. The pastry is made by layering phyllo dough with a mixture of chopped nuts, such as pistachios, walnuts, or almonds, and a sweet syrup or honey. After baking, the pastry is cut into small diamond-shaped pieces and served as a dessert.
+                                   </p>
+                            </div>
                         </div>
                     </Carousel.Item>
                     <Carousel.Item>
-                       <div className='d-flex'>
-                       <img
-                            className="d-block w-50 h-25  rounded  "
-                            src="https://i.ibb.co/zxg77C2/turkish-adana-kebab-720x720.jpg"
-                            alt="Third slide"
-                        />
-                        <div className='px-5 '>
-                            <h3 className='fs-3'>First slide label</h3>
-                            <p className='fs-5'>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                        <div className='d-flex'>
+                            <img
+                                className="d-block w-50 h-25 rounded  "
+                                src="https://i.ibb.co/k3pR4pr/Manti-84d39c9632cf40fcb0ca7625d63556b5.jpg"
+                                alt="Third slide"
+                            />
+
+                            <div className='px-5 '>
+                                <h3  className='fs-3 fw-bold'>Manti</h3>
+                                <p className='fs-5'><span  className='fs-4 fw-bold'>Manti</span> is a traditional Turkish dish that consists of small dumplings typically filled with spiced ground lamb or beef and served with a yogurt sauce and melted butter or tomato sauce. The dumplings are usually made by hand and can be served boiled or baked.</p>
+                            </div>
                         </div>
-                       </div>
+                    </Carousel.Item>
+                    <Carousel.Item>
+                        <div className='d-flex'>
+                            <img
+                                className="d-block w-50 h-25  rounded  "
+                                src="https://i.ibb.co/zxg77C2/turkish-adana-kebab-720x720.jpg"
+                                alt="Third slide"
+                            />
+                            <div className='px-5 '>
+                                <h3  className='fs-3 fw-bold'> Kebab</h3>
+                                <p className='fs-5'><span  className='fs-4 fw-bold'>Kebab</span> is a dish that consists of grilled or roasted meat that is typically served on skewers or with a variety of accompaniments, such as rice, vegetables, and bread. Kebabs are popular in many Middle Eastern, Mediterranean, and South Asian countries, and are often associated with street food and outdoor grilling.</p>
+                            </div>
+                        </div>
                     </Carousel.Item>
 
                     <Carousel.Item>
                         <div className='d-flex'>
-                        <img
-                            className="d-block w-50 h-25  rounded  "
-                            src="https://i.ibb.co/4Tnsmk7/Turkish-Pizza-Lahmacun.jpg"
-                            alt="Third slide"
-                        />
-                        <div className='px-5 '>
-                            <h3 className='fs-3'>First slide label</h3>
-                            <p className='fs-5'>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-                        </div>
+                            <img
+                                className="d-block w-50 h-25  rounded  "
+                                src="https://i.ibb.co/4Tnsmk7/Turkish-Pizza-Lahmacun.jpg"
+                                alt="Third slide"
+                            />
+                            <div className='px-5 '>
+                                <h3  className='fs-3 fw-bold'>Lahmacun</h3>
+                                <p className='fs-5'><span  className='fs-4 fw-bold'>Lahmacun</span> is a popular Middle Eastern and Mediterranean dish that is often referred to as "Turkish pizza". It consists of a thin, round piece of dough that is topped with a mixture of minced meat (usually lamb or beef), onions, tomatoes, herbs, and spices. The dough is then baked in a hot oven until crispy.</p>
+                            </div>
                         </div>
                     </Carousel.Item>
                 </Carousel>
