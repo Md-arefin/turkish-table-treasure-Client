21f1508b20978955e1c3f466c19d8769975babcf
Md-arefin <ansariarefin617@gmail.com>
2023-05-03 16:00:55 +0600

    implemented banner on the home page

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,9 +1,22 @@
 import React from 'react';
 
+
 const Home = () => {
     return (
         <div>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe animi a quo doloribus. Consequuntur natus est explicabo consequatur cumque, id doloribus. Ipsa obcaecati temporibus ad quod veritatis optio, nobis mollitia!</p>
+            <div className='w-auto h-75 position-relative bg-black' >
+                <div>
+                    <img className='img-fluid opacity-50' src="https://i.ibb.co/B2yD0B0/flat-lay-frame-with-grains-stucco-background-86.jpg" alt="" />
+                </div>
+
+                <div className='position-absolute bottom-50 start-50 translate-middle-x text-center text-white w-100 px-5'>
+
+                <h1 className=' my-2 fs-1'>Discover the Rich Flavors of Turkey</h1>
+                <p className=' fs-4 '>
+                The title "Discover the Rich Flavors of Turkey" and the website name TurkishTableTreasure.com perfectly capture the essence of this food recipe website. Visitors to the site will find a treasure trove of Turkish culinary delights, ranging from traditional dishes to modern interpretations. The site is a virtual journey through the tastes of Turkey, offering a sensory exploration of the country's diverse and delicious cuisine. Whether you're an experienced chef or a novice cook, Turkish Table Treasure is the perfect place to discover new flavors and expand your culinary repertoire.
+                </p>
+                </div>
+            </div>
         </div>
     );
 };
